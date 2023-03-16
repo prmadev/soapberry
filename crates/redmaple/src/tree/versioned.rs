d0bd13f6f7b700bc4ad054ebcde7ac96ab62d977
amirhossein alehseikh <amirhossein.alesheikh@gmail.com>
2023-03-16 13:32:34 +0330

    refactor: delete unused versioned trait

--- crates/redmaple/src/tree/versioned.rs
+++
@@ -1,7 +0,0 @@
-/// versioned keeps the version number of the state
-pub trait Versioned {
-    /// returns the version number of the state
-    fn version(&self) -> u64;
-    /// increments the version number of the state
-    fn increment_version(&mut self);
-}
