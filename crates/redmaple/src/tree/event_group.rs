38c78f3f48a150c3f57d3629db6554f272a96007
amirography <amirhossein.alesheikh@gmail.com>
2023-06-19 18:56:05 +0330

    fix: remove obsolete tests

--- crates/redmaple/src/tree/event_group.rs
+++ crates/redmaple/src/tree/event_group.rs
@@ -10,43 +10,6 @@ use super::ID;
 
 /// [`EventGroup`] trait describes the behavior of an event.
 /// Specific implementaiton is not defined here
-///
-/// # Example
-///
-/// ```
-///    use redmaple::event_group::EventGroup;
-///    use std::time::SystemTime;
-///    use thiserror::Error;
-///    use redmaple::tree::{id::ID};
-///
-///
-///
-///    struct Eg(ID, ID, std::time::SystemTime, String);
-///
-///    #[derive(Error, Debug)]
-///    enum EventGroupErrorLocal {
-///    }
-///
-///    impl EventGroup for Eg {
-///        type EventGroupError = EventGroupErrorLocal;
-///
-///        fn id(&self) -> &ID {
-///            &self.0
-///        }
-///
-///
-///        fn time(&self) -> &SystemTime {
-///            &self.2
-///        }
-///    }
-///
-///    let ev1 = Eg(ID::new(uuid::Uuid::new_v4()), ID::new(uuid::Uuid::new_v4()), SystemTime::now(), String::from("first_text"));
-///    let ev2 = Eg(ID::new(uuid::Uuid::new_v4()), ID::new(uuid::Uuid::new_v4()), SystemTime::now(), String::from("second_text"));
-///
-///
-///    // the two instances should not have the same [`ID`]
-///    assert_ne!(ev1.id(), ev2.id());
-/// ```
 pub trait EventGroup {
     /// Errors related to [`EventGroup`]
     type EventGroupError: Error;
--- crates/redmaple/src/tree/id.rs
+++ crates/redmaple/src/tree/id.rs
@@ -1,45 +1,6 @@
 use std::fmt::Display;
 
 /// The Implementation of the ID that the crate uses
-///
-/// # Example
-///
-/// For now, the implementation uses UUID v4 as the inner type.
-///
-/// ```rust
-/// use redmaple::tree::id::ID;
-/// use uuid::Uuid;
-///
-/// let id = Uuid::new_v4();
-///
-/// assert_eq!(id, ID::new(id).inner());
-///
-/// assert_eq!(4usize, ID::new(id).inner().get_version_num());
-/// ```
-///
-/// We should make sure to insure that the `ID::new()` never outputs a nil (all zero) id.
-/// ```
-/// use redmaple::tree::id::ID;
-///
-/// use uuid::Uuid;
-///
-/// let id = Uuid::new_v4();
-///
-/// assert!(!ID::new(id).inner().is_nil());
-/// ```
-///
-/// Of course all ID's should be unique.
-/// ```
-/// use redmaple::tree::id::ID;
-/// use uuid::Uuid;
-///
-/// let id1 = Uuid::new_v4();
-/// let id2 = Uuid::new_v4();
-///
-///
-/// assert_ne!(ID::new(id1), ID::new(id2));
-/// ```
-
 #[derive(
     Default,
     Debug,
